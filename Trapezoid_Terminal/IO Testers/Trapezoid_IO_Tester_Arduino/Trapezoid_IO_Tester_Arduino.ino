22fc082158b78dc03c4b50b2af83e30af95772b6
Forgotten Machines <forgottenmachines@gmail.com>
2023-04-29 04:54:10 -0500

    add datasheets

---
+++ Trapezoid_Terminal/IO Testers/Trapezoid_IO_Tester_Arduino/Trapezoid_IO_Tester_Arduino.ino
@@ -0,0 +1,24 @@
+constexpr int RESTART_ENAB = A7;
+
+
+
+
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello World!");
+}
+void loop() {
+
+
+Serial.println("Hello World!");
+
+
+
+
+
+
+}
