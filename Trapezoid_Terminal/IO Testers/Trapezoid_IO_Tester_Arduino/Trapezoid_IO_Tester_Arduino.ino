a196d43d9ff308fb1705324911a8b63dc29d3399
Forgotten Machines <forgottenmachines@gmail.com>
2023-04-30 01:10:04 -0500

    Update Trapezoid_IO_Tester_Arduino.ino

--- Trapezoid_Terminal/IO Testers/Trapezoid_IO_Tester_Arduino/Trapezoid_IO_Tester_Arduino.ino
+++ Trapezoid_Terminal/IO Testers/Trapezoid_IO_Tester_Arduino/Trapezoid_IO_Tester_Arduino.ino
@@ -2,18 +2,18 @@
 // D/H Data Input 
 // N/R Strobe Input
 
-constexpr int J_Clock_Input = 0;
-constexpr int N_Clock_Input = 1;
-constexpr int D_Data_Input = 2;
-constexpr int H_Data_Input = 3;
-constexpr int N_Strobe_Input = 4;
-constexpr int R_Strobe_Input = 5;
+constexpr int J_Clock_Input = 0; //GREEN 2
+constexpr int P_Clock_Input = 1; //WHT/ORG 1
+constexpr int D_Data_Input = 2; //WHT/BLUE 2
+constexpr int H_Data_Input = 3; //ORANGE
+constexpr int N_Strobe_Input = 4; //WHT/BLUE 1
+constexpr int R_Strobe_Input = 5; //BLUE
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Hello World!");
   pinMode(J_Clock_Input, OUTPUT);  
-  pinMode(N_Clock_Input, OUTPUT);  
+  pinMode(P_Clock_Input, OUTPUT);  
   pinMode(D_Data_Input, OUTPUT);  
   pinMode(H_Data_Input, OUTPUT);  
   pinMode(N_Strobe_Input, OUTPUT);  
@@ -23,7 +23,7 @@ void setup() {
 void loop() {
 
 digitalWrite(J_Clock_Input, LOW);  //keep one side of the pair low all the time
-digitalWrite(N_Clock_Input, LOW);
+digitalWrite(P_Clock_Input, LOW);
 
 digitalWrite(D_Data_Input, LOW);  
 digitalWrite(H_Data_Input, LOW);  
@@ -32,9 +32,9 @@ digitalWrite(N_Strobe_Input, LOW);
 digitalWrite(R_Strobe_Input, LOW); 
 
 //Pulse the clock
-digitalWrite(N_Clock_Input, LOW);
+digitalWrite(P_Clock_Input, LOW);
   delay(10);
-digitalWrite(N_Clock_Input, HIGH);  
+digitalWrite(P_Clock_Input, HIGH);  
   delay(10);
 
 
