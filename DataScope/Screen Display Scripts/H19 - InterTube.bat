d69a4fae36f83f6c0df619c78745f1997b7c1325
Forgotten Machines <forgottenmachines@gmail.com>
2024-12-15 18:39:47 -0600

    lots

---
+++ DataScope/Screen Display Scripts/H19 - InterTube.bat
@@ -0,0 +1,12 @@
+:start
+type RR-Logos-BIG.txt >\\.\COM13
+type RR-Logos-BIG.txt >\\.\COM12
+type StarWarsRetroRecipes.txt >\\.\COM13
+type StarWarsRetroRecipes.txt >\\.\COM12
+type RR-Logos-BIG-R.txt >\\.\COM13
+type RR-Logos-BIG-R.txt >\\.\COM12
+type StarWarsRetroRecipes.txt >\\.\COM13
+type StarWarsRetroRecipes.txt >\\.\COM12
+timeout /t 1 /nobreak
+goto start
+
---
+++ DataScope/Screen Display Scripts/H19-InterTube.bat
@@ -0,0 +1,19 @@
+:start
+type RR-Logos-BIG.txt >\\.\COM13
+timeout /t 2 /nobreak
+type RR-Logos-BIG.txt >\\.\COM12
+timeout /t 2 /nobreak
+type StarWarsRetroRecipes.txt >\\.\COM13
+timeout /t 2 /nobreak
+type StarWarsRetroRecipes.txt >\\.\COM12
+timeout /t 2 /nobreak
+type RR-Logos-BIG-R.txt >\\.\COM13
+timeout /t 2 /nobreak
+type RR-Logos-BIG-R.txt >\\.\COM12
+timeout /t 2 /nobreak
+type StarWarsRetroRecipes.txt >\\.\COM13
+timeout /t 2 /nobreak
+type StarWarsRetroRecipes.txt >\\.\COM12
+timeout /t 2 /nobreak
+goto start
+
