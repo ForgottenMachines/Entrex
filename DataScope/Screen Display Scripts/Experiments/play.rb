17e021e488b7653c625c60957ecd9cad1e2e3a0b
Forgotten Machines <forgottenmachines@gmail.com>
2025-09-09 03:38:42 -0500

    Update play.rb

--- DataScope/Screen Display Scripts/Experiments/play.rb
+++ DataScope/Screen Display Scripts/Experiments/play.rb
@@ -7,7 +7,7 @@ file_path="DataScopeVCFMW_unix.txt"
 
 # Delay (in seconds) between characters & Lines
 delay_between_chars=0.001
-delay_between_lines=0.15
+delay_between_lines=0.4
 
 # Configure the serial port settings
 `stty -F "#{serial_port}" "#{baud_rate}" cs8 -cstopb -parenb`
